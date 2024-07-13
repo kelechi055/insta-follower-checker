4fff46af5ba54a46bdd428438e357dd3a77b9e13
kelechi055 <157651060+kelechi055@users.noreply.github.com>
2024-07-13 13:19:08 -0400

    Update checker.js

--- checker.js
+++ checker.js
@@ -1,7 +1,7 @@
 //Make sure the user is on instagram.com
 
 if (window.location.origin !== "https://www.instagram.com") {
-  alert("Make sure you're on Instagram. Redirecting to Instagram... You have to run the code in the console again.");
+  alert("Make sure you're on Instagram. Redirecting to Instagram... Copy, Paste and run this in console again.");
   window.location.assign("https://www.instagram.com");
   console.clear();
 }

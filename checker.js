5c53bd972eb9c5e44a8e3611a30516271ca13ab1
kelechi055 <157651060+kelechi055@users.noreply.github.com>
2024-07-12 20:01:36 -0400

    Create checker.js

---
+++ checker.js
@@ -0,0 +1,7 @@
+if (window.location.origin !== "https://www.instagram.com") {
+  window.alert(
+    "Make sure youre on instagram. Redirecting to instagram...You have to run the code into the console again.",
+  );
+  window.location.href = "https://www.instagram.com";
+  console.clear();
+}

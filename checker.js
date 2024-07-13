ae66cfd39b57ebee0d09623ee81e99c0753e1b19
kelechi055 <157651060+kelechi055@users.noreply.github.com>
2024-07-12 20:03:25 -0400

    Update checker.js

--- checker.js
+++ checker.js
@@ -1,3 +1,5 @@
+//Make sure the user is on instagram
+
 if (window.location.origin !== "https://www.instagram.com") {
   window.alert(
     "Make sure youre on instagram. Redirecting to instagram...You have to run the code into the console again.",
@@ -5,3 +7,13 @@ if (window.location.origin !== "https://www.instagram.com") {
   window.location.href = "https://www.instagram.com";
   console.clear();
 }
+
+const fetchOptions = {
+  credentials: "include",
+  headers: {
+    "X-IG-App-ID": "936619743392459",
+  },
+  method: "GET",
+};
+
+let username;

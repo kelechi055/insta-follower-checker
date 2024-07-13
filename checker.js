840dff566302a4f88651796f365ffc2d2cf35bc8
kelechi055 <157651060+kelechi055@users.noreply.github.com>
2024-07-12 21:23:32 -0400

    Update checker.js

--- checker.js
+++ checker.js
@@ -1,19 +1,17 @@
-//Make sure the user is on instagram
+//Make sure the user is on instagram.com
 
 if (window.location.origin !== "https://www.instagram.com") {
-  window.alert(
-    "Make sure youre on instagram. Redirecting to instagram...You have to run the code into the console again.",
-  );
-  window.location.href = "https://www.instagram.com";
+  alert("Make sure you're on Instagram. Redirecting to Instagram... You have to run the code in the console again.");
+  window.location.assign("https://www.instagram.com");
   console.clear();
 }
 
 const fetchOptions = {
-  credentials: "include",
-  headers: {
-    "X-IG-App-ID": "936619743392459",
-  },
   method: "GET",
+  headers: {
+    "X-IG-App-ID": "936619743392459"
+  },
+  credentials: "include"
 };
 
 let username;
